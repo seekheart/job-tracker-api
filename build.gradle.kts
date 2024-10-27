ab57ba97de8dea38d865df08879606320c324c53
Mike Tung <miketung2013@gmail.com>
2024-10-27 14:06:47 -0400

    removed mapstruct from build gradle
    - removed kapt annotation processor, mapstruct library and deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
 }
 
 kotlin {
