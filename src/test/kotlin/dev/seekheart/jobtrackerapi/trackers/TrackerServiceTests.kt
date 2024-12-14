36026555b5fcc2e4e3704ec376cb5081659f5d8a
Mike Tung <miketung2013@gmail.com>
2024-12-14 16:17:29 -0500

    adding more tracker tests
    - added tests for saving trackers to user accounts

--- src/test/kotlin/dev/seekheart/jobtrackerapi/trackers/TrackerServiceTests.kt
+++ src/test/kotlin/dev/seekheart/jobtrackerapi/trackers/TrackerServiceTests.kt
@@ -1,11 +1,15 @@
 package dev.seekheart.jobtrackerapi.trackers
 
 import dev.seekheart.jobtrackerapi.trackers.models.Tracker
+import dev.seekheart.jobtrackerapi.trackers.models.TrackerNameAlreadyExistsException
+import dev.seekheart.jobtrackerapi.trackers.models.toPayload
 import dev.seekheart.jobtrackerapi.trackers.repositories.TrackerRepository
 import dev.seekheart.jobtrackerapi.trackers.services.TrackerService
+import dev.seekheart.jobtrackerapi.users.models.UserNotFoundException
 import dev.seekheart.jobtrackerapi.users.repositories.UserRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
@@ -26,7 +30,7 @@ class TrackerServiceTests {
 
 
     @Test
-    fun test_getTrackersbyUserId_should_return_trackers() {
+    fun test_getTrackersByUserId_should_return_trackers() {
         val userId = UUID.randomUUID()
         val trackers = listOf<Tracker>(
             Tracker(owner = userId, name = "test a"),
@@ -38,5 +42,48 @@ class TrackerServiceTests {
 
         val result = trackerService.getTrackersByUserId(userId)
         assertEquals(trackers.size, result.size)
+    }
+
+    @Test
+    fun test_saveTrackerToUser_should_return_tracker_attached_to_user() {
+        val userId = UUID.randomUUID()
+        val tracker = Tracker(owner = userId, name = "test a")
+        val payload = tracker.toPayload()
+
+
+        `when`(userRepository.existsById(userId)).thenReturn(true)
+        `when`(trackerRepository.existsByName("test a")).thenReturn(false)
+        `when`(trackerRepository.save(tracker)).thenReturn(tracker)
+
+        val result = trackerService.saveTrackerToUser(userId, payload)
+
+        assertEquals(result, payload)
+    }
+
+    @Test
+    fun test_saveTrackerToUser_should_throw_UserNotFoundException() {
+        val userId = UUID.randomUUID()
+        val tracker = Tracker(owner = userId, name = "test a")
+        val payload = tracker.toPayload()
+
+        `when`(userRepository.existsById(userId)).thenReturn(false)
+
+        assertThrows<UserNotFoundException> {
+            trackerService.saveTrackerToUser(userId, payload)
+        }
+    }
+
+    @Test
+    fun test_saveTrackerToUser_should_throw_TrackerNameAlreadyExistsException() {
+        val userId = UUID.randomUUID()
+        val tracker = Tracker(owner = userId, name = "test a")
+        val payload = tracker.toPayload()
+
+        `when`(userRepository.existsById(userId)).thenReturn(true)
+        `when`(trackerRepository.existsByName("test a")).thenReturn(true)
+
+        assertThrows<TrackerNameAlreadyExistsException> {
+            trackerService.saveTrackerToUser(userId, payload)
+        }
     }
 }
